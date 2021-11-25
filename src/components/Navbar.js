82adf5786c40a99545d794e733c8ab352a0cb8f3
Tom Oliveira <tom.oliveira@ymail.com>
2021-11-25 16:53:08 +0100

    corrections

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,16 +6,12 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
-import titre from './titre.png'
+import titre from '../images/titre.png'
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
