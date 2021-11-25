3bfa676aa009e3ae119df15787e49a71464d2d42
Tom Oliveira <tom.oliveira@ymail.com>
2021-11-25 16:46:25 +0100

    images

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import './App.css';
 import PrimarySearchAppBar from './components/Navbar'
 import Widget_Attaque from './Widgets/Widget_Attaque';
+import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 
 function App() {
       return(
-        <div class ="container">
-          <PrimarySearchAppBar/>
-          <div class = "row">
+        <Container fluid classname="fondpk">
 
-          <div class = "col-12 center">
-           {/* <img src={titre}/> */}
+          <PrimarySearchAppBar/>
+
+            <Row>
+
            <div className ="mono">
              zbeb
            </div>
-           zbeb
-          </div>
-            <div class = "col-12 center">
+
+              <Col xs={6} md={6} lg={6} xl={6}>
               
               <Widget_Attaque/>
 
-            </div>
-          </div>
-        </div>
+              </Col>
+              <Col xs={6} md={6} lg={6} xl={6}>
+              
+              <Widget_Attaque/>
+
+              </Col>
+
+            </Row>
+          </Container>
       );
 }
 
--- src/Widgets/LigneAtk.js
+++ src/Widgets/LigneAtk.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import './LigneAtk.css';
 import {Row} from 'react-bootstrap';
 import {Col} from 'react-bootstrap';
-import pika from './pika.png';
 import './LigneAtk.css';
 
 
-export default class DescriptionEquipe extends React.Component {
+export default class LigneAtk extends React.Component {
     
     render() {
         return(            
             <div className="contour">
                 <Row>
                     <Col xs={6} >
-                        <img src={pika.png} alt="image du pok" className="plein"></img>
+                    <img src={this.props.logo} alt="logoEquipe" className="plein"></img>
                     </Col>
                     <Col xs={6}>
                         <div className="descriptionNom">
                             Nom
                         </div>
-                        <div className="descriptionConf">
-                            conférence
+                        <div className="stat">
+                            Stat0
                         </div>
                     </Col>
                 </Row>
